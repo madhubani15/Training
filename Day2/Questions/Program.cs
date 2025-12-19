f48cf5d51b7e7d520ca025b79ff1677764cfeef4
Mandabi <119072379+madhubani15@users.noreply.github.com>
2025-12-19 16:53:17 +0530

    Add files via upload

---
+++ Day2/Questions/Program.cs
@@ -0,0 +1,9 @@
+﻿// See https://aka.ms/new-console-template for more information
+using Questions;
+
+Console.WriteLine("Hello, World!");
+Height.CheckHeight();
+LargestOfThree.Check();
+LeapYearChecker.Check();
+QuadraticEquations.Calculate();
+AdmissionEligibility.Validate();
