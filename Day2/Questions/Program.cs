c526477b8d741ccec63ed6a63c43faa53258993e
Mandabi <119072379+madhubani15@users.noreply.github.com>
2025-12-21 23:20:16 +0530

    Delete Day2/Questions directory

--- Day2/Questions/Program.cs
+++
@@ -1,9 +0,0 @@
-﻿// See https://aka.ms/new-console-template for more information
-using Questions;
-
-Console.WriteLine("Hello, World!");
-Height.CheckHeight();
-LargestOfThree.Check();
-LeapYearChecker.Check();
-QuadraticEquations.Calculate();
-AdmissionEligibility.Validate();
