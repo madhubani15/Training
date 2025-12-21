9a1cfd806a76451ead6083442df396f65a28ddcf
Mandabi <119072379+madhubani15@users.noreply.github.com>
2025-12-21 23:26:53 +0530

    Update program.cs

--- Day1/program.cs
+++ Day1/program.cs
@@ -1,36 +1,3 @@
-﻿/*
- using System;
-
-class Program
-{
-    static void Main()
-    {
-        int n = 100;
-        bool[] isPrime = new bool[n + 1];
-
-        for (int i = 3; i <= n; i += 2)
-            isPrime[i] = true;
-
-        isPrime[2] = true;
-
-        for (int i = 3; i * i <= n; i += 2)
-        {
-            if (isPrime[i])
-            {
-                for (int j = i * i; j <= n; j += 2 * i)
-                    isPrime[j] = false;
-            }
-
-        }
-
-        Console.Write("2 ");
-        for (int i = 3; i <= n; i += 2)
-        {
-            if (isPrime[i])
-                Console.Write(i + " ");
-        }
-    }
-}*/
 
 
 using System;
