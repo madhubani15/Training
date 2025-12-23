22a538a953a5bc7d5a7d2d08b20e52cc4c8f307b
Mandabi <119072379+madhubani15@users.noreply.github.com>
2025-12-23 12:13:54 +0530

    Create Computer.cs

---
+++ Day5/Assignments/Question1/Computer.cs
@@ -0,0 +1,16 @@
+using System;
+namespace Assignments;
+
+public class Computer
+{
+    
+    #region properties
+    public string Processor{ get; set; }
+    public int RamSize { get; set; }
+    public int HardDiskSize { get; set; }
+    public int GraphicCard { get; set; }
+    #endregion
+
+   
+
+}
