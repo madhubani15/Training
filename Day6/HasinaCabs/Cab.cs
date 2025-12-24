de0d7965a021abe3dc292878f9ef25a50020099c
Mandabi <119072379+madhubani15@users.noreply.github.com>
2025-12-24 15:56:16 +0530

    Add Cab class with properties for cab details

---
+++ Day6/HasinaCabs/Cab.cs
@@ -0,0 +1,16 @@
+using System;
+
+namespace HasinaCabs;
+
+public class Cab
+{
+    #region properties
+    public string CabType { get; set; }
+    public string BookingID { get; set; }
+    public int WaitingTime { get; set; }
+    public double Distance{ get; set; }
+
+    #endregion
+
+    
+}
