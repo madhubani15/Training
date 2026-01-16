9ae841fc7d080bdf50aee5969d1b6d1ed9482986
Mandabi <119072379+madhubani15@users.noreply.github.com>
2026-01-16 09:53:23 +0530

    Add files via upload

---
+++ Day9/LPUMgmtSystem/LPU_Exceptions/LPUException.cs
@@ -0,0 +1,17 @@
+﻿namespace LPU_Exceptions
+{
+    /// <summary>
+    /// custom exception class created for LPU Management System
+    /// By Mandabi Mandal on Date 29/12/2025 at 11:34 AM
+    /// </summary>
+    public class LPUException : Exception
+    {
+        public LPUException() :base() { 
+        
+        }
+
+        public LPUException(string ErrorMsg) : base(ErrorMsg) { 
+        
+        }
+    }
+}
