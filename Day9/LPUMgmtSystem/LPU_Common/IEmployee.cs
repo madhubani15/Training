8d15ebf447a7c5286b3e4d222ff5fa22f2f1b87d
Mandabi <119072379+madhubani15@users.noreply.github.com>
2026-01-16 09:50:06 +0530

    Add files via upload

---
+++ Day9/LPUMgmtSystem/LPU_Common/IEmployee.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LPU_Common
+{
+
+    interface IEmployee<T> : IRepo<T>
+    {
+        List<T> ShowAllFemaleEmployees();
+        List<T> ShowAllMaleEmployees();
+        List<T> ShowAllEmployeesWithAgeAbove40();
+        
+    }
+}
---
+++ Day9/LPUMgmtSystem/LPU_Common/IStudentCRUD.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using LPU_Entity;
+using System.Collections.Generic;
+namespace LPU_Common
+{
+    public interface IStudentCRUD
+    {
+        /// <summary>
+        /// Interface is for service oriented applications
+        /// Whenever this interface will be implemented, all the abstract methods will HAVE TO BE
+        /// overriden. It is and enforcement
+        /// </summary>
+        Student SearchStudentByID(int ID);
+        List<Student> SearchStudentByName(string name);
+        bool EnrollStudent(Student sObj);
+        bool UpdateStudentDetails(int id, Student newObj);
+        bool DropStudentDetails(int id);
+    }
+}
