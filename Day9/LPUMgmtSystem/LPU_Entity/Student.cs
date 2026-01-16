e4459aa410ea8cb96a26df45fbb2c094e0faf3ca
Mandabi <119072379+madhubani15@users.noreply.github.com>
2026-01-16 09:51:52 +0530

    Add files via upload

---
+++ Day9/LPUMgmtSystem/LPU_Entity/Student.cs
@@ -0,0 +1,30 @@
+﻿namespace LPU_Entity
+{
+    public enum CourseType
+    {
+        // To make sure that the user can enter only these specific courses
+        Mechanical=10,
+        Electrical=20,
+        Civil=30,
+        CSE=40,
+        IT=50
+    }
+    public class Student // Entity class: Student is an entity
+    {
+        #region properties
+
+        public int StudentID { get; set; }
+        // properties never leave footprint
+        public string Name { get; set; }
+
+        // public string Course { get; set; }
+        // this property requires validation because users can fill inferior data
+        // in UI a drop down list will work better
+        // Here, we can't use dropdown so we'll use enum (named integer constant)
+
+        public CourseType Course { get; set; } // Enum type property
+        public string Address { get; set; }
+
+        #endregion
+    }
+}
