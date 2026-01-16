05ea9bcd68062e7bd240ed903f9c6eac88d68886
Mandabi <119072379+madhubani15@users.noreply.github.com>
2026-01-16 10:01:36 +0530

    Add files via upload

---
+++ Day12/OperatorOverloading/Employee.cs
@@ -0,0 +1,17 @@
+using System;
+
+namespace OperatorOverLoadingDemo;
+
+public class Employee
+{
+    public int EmpID { get; set; }
+    public string EmpName { get; set; }
+    public int BSalary { get; set; }
+
+    public static Employee operator +(Employee e1, Employee e2)
+    {
+        Employee temp = new Employee();
+        temp.BSalary = e1.BSalary +e2.BSalary;
+        return temp;
+    }
+}
---
+++ Day12/OperatorOverloading/Program.cs
@@ -0,0 +1,35 @@
+﻿// See https://aka.ms/new-console-template for more information
+//Console.WriteLine("Hello, World!");
+
+using OperatorOverLoadingDemo;
+
+int num1=100;
+int num2=200;
+int numResult=num1+num2;
+
+
+System.Console.WriteLine(numResult);
+
+Employee emp1=new Employee();
+emp1.EmpID=101;
+emp1.EmpName="Alok";
+emp1.BSalary=40000;
+
+
+Employee emp2=new Employee();
+emp1.EmpID=102;
+emp1.EmpName="Riya";
+emp1.BSalary=35000;
+
+Employee emp3=new Employee();
+emp1.EmpID=1789;
+emp1.EmpName="Manjeet";
+emp1.BSalary=65000;
+// int numResult=num1+num2;
+Employee empObj = emp1+emp2+emp3;
+
+System.Console.WriteLine($"Total salary paid : {empObj.BSalary}");
+
+
+MyLogic m1 = new MyLogic();
+m1.
