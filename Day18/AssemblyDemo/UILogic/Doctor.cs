25cf136ff4b53faa1c3ad7b2c969d999fbd6f449
Mandabi <119072379+madhubani15@users.noreply.github.com>
2026-01-16 10:38:39 +0530

    Add files via upload

---
+++ Day18/AssemblyDemo/UILogic/Doctor.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ServiceLogic
+{
+    //If it only can be applied on field
+    //[AttributeUsage(AttributeTargets.Field)]
+    //It shows where it will be applied
+    [AttributeUsage(AttributeTargets.Class | AttributeTargets.Property | AttributeTargets.Method, AllowMultiple =true)]
+    public class DoctorAttribute : Attribute
+    {
+        #region properties
+        public string Name { get; set; }
+        public string CheckedOnDate { get; set; }
+        #endregion
+    }
+}
---
+++ Day18/AssemblyDemo/UILogic/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ServiceLibrary;
+using ServiceLogic;
+
+namespace UILogic
+{
+    [Doctor(Name = "Neha", CheckedOnDate ="12/04/2025")]
+    [Doctor(Name = "Ravi", CheckedOnDate = "6/03/2025")]
+    [Doctor(Name = "Shashi", CheckedOnDate = "12/02/2025")]
+    [Serializable]
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            int num1;
+            int num2;
+            Console.Write("Enter First Number: ");
+            num1 = Convert.ToInt32(Console.ReadLine());
+
+            Console.Write("Enter Second Number: ");
+            num2 = Convert.ToInt32(Console.ReadLine());
+
+            SomeLogic logic = new SomeLogic();
+            int numResult = logic.AddMe(num1, num2);
+            Console.WriteLine($"The Sum Of {num1} And {num2} is {numResult}");
+            Console.ReadLine();
+
+
+        }
+    }
+}
