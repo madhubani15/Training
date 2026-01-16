f732044b4f86844bece41c16ad57f6e0b49c946e
Mandabi <119072379+madhubani15@users.noreply.github.com>
2026-01-16 09:37:09 +0530

    Add files via upload

---
+++ Day8/FileIoDemo/DirectoryDemo.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.IO;
+namespace FileIODemo;
+
+public class DirectoryDemo
+{
+    public void DirectoryDemoFunc(string directoryName)
+    {
+        if (Directory.Exists(directoryName))
+        {
+            Console.WriteLine("Folder already exists");
+        }
+        else
+        {
+            Directory.CreateDirectory(directoryName);
+            Console.WriteLine("Folder Created...");
+        }
+    }
+
+    public void DriveInfoFunc(string driveName) {
+        DriveInfo dInfo = new DriveInfo(driveName);
+        Console.WriteLine($"Drive Name : {dInfo.Name}");
+        Console.WriteLine($"Drive FileSystem : {dInfo.DriveType}");
+        Console.WriteLine($"Drive Size : {dInfo.TotalSize}");
+        Console.WriteLine($"Drive Free Space : {dInfo.AvailableFreeSpace}");
+    }
+
+    public void PathDemoFunc()
+    {
+        string s = @"C:\Users\hp\OneDrive\Desktop\.NET.docx";
+        Console.WriteLine(Path.GetFullPath(s));
+        //Console.WriteLine(Path.GetTempPath(s));
+
+    }
+}
---
+++ Day8/FileIoDemo/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+namespace FileIODemo;
+
+class Program
+{
+    static void Main()
+    {
+        DirectoryDemo dObj = new DirectoryDemo();
+        dObj.DirectoryDemoFunc("LPU");
+
+        //dObj.DriveInfoFunc("D:\\"); // "\\" : Path seperator because "\" is escape sequence
+
+        FileStreamDemo fsDemoObj = new FileStreamDemo();
+        fsDemoObj.CreateFile(@"C:\Users\hp\Training\Day8\FileIoDemo\FileIoDemo\bin\Debug\net10.0\LPU\SampleData.txt");
+
+        fsDemoObj.ReadFile(@"C:\Users\hp\Training\Day5\CakeWorld\Cake.cs");
+    }
+}
