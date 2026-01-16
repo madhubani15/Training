1f27d13b15464f0554aaadc2712f72d863bedad1
Mandabi <119072379+madhubani15@users.noreply.github.com>
2026-01-16 09:35:30 +0530

    Add files via upload

---
+++ Day8/EventDelegateDemo/Cutsomer.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CustomPropertyDemo
+{
+    class Customer
+    {
+        // Orders[] myOrders = null;
+        List<Orders> OrderList; // Dynamic
+        // attributes are never public
+        public Customer()
+        {
+            //myOrders = new Orders[5];
+            OrderList = new List<Orders>();
+        }
+        public int CustID { get; set; } //CLR properties
+        // IN CLR => Auto Implicit Properties
+        // read only properties are known as default properties
+        public string Name { get; set; }
+        public Address BillingAddress { get; set; }
+        public Address ShippingAddress { get; set; }
+        public List<Orders> MyOrders {
+            get
+            {
+                return OrderList;
+            }
+            protected set // cannot set this property without a child class
+            // nobody can modify this
+            {
+                OrderList = value;
+            }
+        }
+
+        // loose coupling => interface
+    }
+}
---
+++ Day8/EventDelegateDemo/Orders.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CustomPropertyDemo
+{
+    internal class Orders
+    {
+        public int OrderID { get; set; }
+        public DateTime OrderDate { get; set; }
+        public int Amount { get; set; }
+
+    }
+}
