cff6da305109d5d8957756067598a817deb47811
Mandabi <119072379+madhubani15@users.noreply.github.com>
2026-01-16 10:41:11 +0530

    Add files via upload

---
+++ Day19/WinReflectionDemo/Form1.Designer.cs
@@ -0,0 +1,137 @@
+﻿namespace WinReflectionDemo
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.listBox2 = new System.Windows.Forms.ListBox();
+            this.lblProperties = new System.Windows.Forms.Label();
+            this.lblMethods = new System.Windows.Forms.Label();
+            this.btnLoadAssembly = new System.Windows.Forms.Button();
+            this.btnClearAll = new System.Windows.Forms.Button();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.SuspendLayout();
+            // 
+            // listBox1
+            // 
+            this.listBox1.Font = new System.Drawing.Font("Gadugi", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.HorizontalScrollbar = true;
+            this.listBox1.ItemHeight = 28;
+            this.listBox1.Location = new System.Drawing.Point(52, 80);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(283, 340);
+            this.listBox1.TabIndex = 0;
+            // 
+            // listBox2
+            // 
+            this.listBox2.Font = new System.Drawing.Font("Gadugi", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.listBox2.FormattingEnabled = true;
+            this.listBox2.HorizontalScrollbar = true;
+            this.listBox2.ItemHeight = 28;
+            this.listBox2.Location = new System.Drawing.Point(458, 80);
+            this.listBox2.Name = "listBox2";
+            this.listBox2.Size = new System.Drawing.Size(283, 340);
+            this.listBox2.TabIndex = 1;
+            // 
+            // lblProperties
+            // 
+            this.lblProperties.AutoSize = true;
+            this.lblProperties.Font = new System.Drawing.Font("Gadugi", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblProperties.Location = new System.Drawing.Point(47, 31);
+            this.lblProperties.Name = "lblProperties";
+            this.lblProperties.Size = new System.Drawing.Size(130, 28);
+            this.lblProperties.TabIndex = 2;
+            this.lblProperties.Text = "Properties";
+            // 
+            // lblMethods
+            // 
+            this.lblMethods.AutoSize = true;
+            this.lblMethods.Font = new System.Drawing.Font("Gadugi", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblMethods.Location = new System.Drawing.Point(453, 31);
+            this.lblMethods.Name = "lblMethods";
+            this.lblMethods.Size = new System.Drawing.Size(112, 28);
+            this.lblMethods.TabIndex = 3;
+            this.lblMethods.Text = "Methods";
+            // 
+            // btnLoadAssembly
+            // 
+            this.btnLoadAssembly.Font = new System.Drawing.Font("Gadugi", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnLoadAssembly.Location = new System.Drawing.Point(861, 80);
+            this.btnLoadAssembly.Name = "btnLoadAssembly";
+            this.btnLoadAssembly.Size = new System.Drawing.Size(198, 60);
+            this.btnLoadAssembly.TabIndex = 4;
+            this.btnLoadAssembly.Text = "Load Assembly";
+            this.btnLoadAssembly.UseVisualStyleBackColor = true;
+            this.btnLoadAssembly.Click += new System.EventHandler(this.btnLoadAssembly_Click);
+            // 
+            // btnClearAll
+            // 
+            this.btnClearAll.Font = new System.Drawing.Font("Gadugi", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnClearAll.Location = new System.Drawing.Point(861, 194);
+            this.btnClearAll.Name = "btnClearAll";
+            this.btnClearAll.Size = new System.Drawing.Size(198, 60);
+            this.btnClearAll.TabIndex = 5;
+            this.btnClearAll.Text = "Clear All";
+            this.btnClearAll.UseVisualStyleBackColor = true;
+            this.btnClearAll.Click += new System.EventHandler(this.btnClearAll_Click);
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1091, 556);
+            this.Controls.Add(this.btnClearAll);
+            this.Controls.Add(this.btnLoadAssembly);
+            this.Controls.Add(this.lblMethods);
+            this.Controls.Add(this.lblProperties);
+            this.Controls.Add(this.listBox2);
+            this.Controls.Add(this.listBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.ListBox listBox2;
+        private System.Windows.Forms.Label lblProperties;
+        private System.Windows.Forms.Label lblMethods;
+        private System.Windows.Forms.Button btnLoadAssembly;
+        private System.Windows.Forms.Button btnClearAll;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+    }
+}
+
---
+++ Day19/WinReflectionDemo/Form1.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Reflection;
+
+namespace WinReflectionDemo
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnClearAll_Click(object sender, EventArgs e)
+        {
+            listBox1.Items.Clear(); // instance
+            listBox2.Items.Clear();
+        }
+
+        private void btnLoadAssembly_Click(object sender, EventArgs e)
+        {
+            openFileDialog1.ShowDialog();
+            string FileName = openFileDialog1.FileName;
+            // stores the path of the file in FileName
+
+            Assembly assemblyObj = Assembly.LoadFrom(FileName);
+            // loads a .NET assembly (DLL or EXE) into your application at
+            // runtime from the path specified by FileName
+
+            Type[] myType = assemblyObj.GetTypes();
+            // types of assembly
+
+            this.ReflectAll(myType[0]);
+        }
+
+
+        // static   : Maintains one copy per class, shared by all objects
+        // instance : Maintains one copy per object, aka multiple copies for multiple objects
+
+        public void ReflectAll(Type typeObj) // reflection
+        {
+            // Getting all methods
+            MethodInfo[] methodList = typeObj.GetMethods();
+
+            // getting all properties
+            PropertyInfo[] propertyList = typeObj.GetProperties();
+
+            // event info
+            //EventInfo[] eventList = type
+            //FieldInfo
+            //DelegateInfo
+            //ConstructorInfo
+
+
+            // Load All Properties
+            foreach (var item in propertyList)
+            {
+                listBox1.Items.Add(item);
+            }
+
+            // Load All Methods
+            foreach (var item in methodList)
+            {
+                listBox2.Items.Add(item);
+            }
+
+        }
+
+    }
+}
